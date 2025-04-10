76fea2187c9f48e7494f7d4c4e5b43bba671311b
victoriia06 <k.kim221b@gmail.com>
2025-04-10 10:27:56 +0300

    Update table.sql

--- table.sql
+++ table.sql
@@ -1,15 +1,34 @@
-CREATE TABLE my_table (
-id INT(10) unsigned NOT NULL AUTO_INCREMENT,
-fio VARCHAR(128),
-tel NUMBER(30),
-email VARCHAR(30),
-gender VARCHAR(10),
-biography TEXT,
-PRIMARY KEY(id)
-);
+-- Create applications table
+CREATE TABLE applications (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    fio VARCHAR(150) NOT NULL,
+    tel VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    birth_date DATE NOT NULL,
+    gender ENUM('male', 'female') NOT NULL,
+    bio TEXT NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE plang(
-id INT(10) unsigned NOT NULL AUTO_INCREMENT,
-name VARCHAR(128),
-PRIMARY KEY(id)
-);
+-- Create programming_languages table
+CREATE TABLE programming_languages (
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id),
+    UNIQUE KEY (name)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Create application_languages junction table
+CREATE TABLE application_languages (
+    application_id INT UNSIGNED NOT NULL,
+    language_id INT UNSIGNED NOT NULL,
+    PRIMARY KEY (application_id, language_id),
+    FOREIGN KEY (application_id) REFERENCES applications(id) ON DELETE CASCADE,
+    FOREIGN KEY (language_id) REFERENCES programming_languages(id) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Insert initial programming languages
+INSERT INTO programming_languages (name) VALUES 
+('Pascal'), ('C'), ('C++'), ('JavaScript'), ('PHP'), 
+('Python'), ('Java'), ('Haskell'), ('Clojure'), 
+('Prolog'), ('Scala'), ('Go');
